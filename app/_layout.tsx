76ea5f1281d1a2f6a62cea2f76e61bfba5f7aa28
Mokafs <155798139+mokafs@users.noreply.github.com>
2025-09-14 16:17:03 +0800

    app start on loginpage and placeholder credentials

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,24 @@
 import { Stack } from "expo-router";
+import { useState } from "react";
+import LoginScreen from './LoginPage';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Pass a prop to LoginScreen to update login state
+  if (!isLoggedIn) {
+    return <LoginScreen onLogin={() => setIsLoggedIn(true)} />;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen
         name="(tabs)"
         options={{
           headerShown: false,
         }}
-    />
-  </Stack>;
+      />
+    </Stack>
+  );
 }
